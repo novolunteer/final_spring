b60c4f75ca48fbf44a079c1a0db89a883ffb675f
novolunteeer <novolunteeer@gmail.com>
2026-03-25 18:08:00 +0900

    로그인 준비 완

---
+++ src/main/java/com/example/demo/auth/dto/PatientUserResponse.java
@@ -0,0 +1,40 @@
+package com.example.demo.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class PatientUserResponse {
+    private Long patientId;
+    private Long userId;
+    private String rrn;
+    private String name;
+    private String phone;
+    private String address;
+    private String gender;
+    private String bloodType;
+    private Float height;
+    private Float weight;
+    private String email;
+    private String password;
+    private LocalDateTime createdAt;
+    private String status;
+
+    public Map<String, Object> getClaims(){
+        Map<String, Object> data=new HashMap<>();
+        data.put("patientId",patientId);
+        data.put("rrn",rrn);
+        data.put("name",name);
+        data.put("email",email);
+        data.put("password",password);
+        return data;
+    }
+}

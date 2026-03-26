0619005775d96ca6645fb63ce2ef475450ca8a4e
vlria <shslvllria540@gmail.com>
2026-03-26 12:23:08 +0900

    reservation Service

--- src/main/java/com/example/demo/patient/PatientRepository.java
+++ src/main/java/com/example/demo/patient/PatientRepository.java
@@ -1,6 +1,8 @@
 package com.example.demo.patient;
 
+import com.example.demo.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Patient findByUser(User user);
 }

d73f9e792c4b6bb725c91b2443407ba51184d2da
vlria <shslvllria540@gmail.com>
2026-03-26 12:23:08 +0900

    reservation Service

---
+++ src/main/java/com/example/demo/reservation/Reservation.java
@@ -0,0 +1,36 @@
+package com.example.demo.reservation;
+
+import com.example.demo.patient.Patient;
+import com.example.demo.staff.Staff;
+import com.example.demo.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reservationId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "patientId")
+    private Patient patient;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "staffId")
+    private Staff staff;
+
+    private LocalDateTime reservationDatetime;
+    private String status;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}

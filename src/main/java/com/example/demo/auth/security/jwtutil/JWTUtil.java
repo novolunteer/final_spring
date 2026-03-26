f715da1006fa4f5e24f734d151ca926c3dd9935d
novolunteeer <novolunteeer@gmail.com>
2026-03-26 09:55:27 +0900

    security

---
+++ src/main/java/com/example/demo/auth/security/jwtutil/JWTUtil.java
@@ -0,0 +1,15 @@
+package com.example.demo.auth.security.jwtutil;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+
+@Component
+public class JWTUtil {
+//    private final SecretKey key;
+//
+//    public JWTUtil(@Value() String secretKey){
+//
+//    }
+}

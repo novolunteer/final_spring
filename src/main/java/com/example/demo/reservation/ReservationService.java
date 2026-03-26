859432d53e59763e4eb650f0153afe9f8707ebcd
vlria <shslvllria540@gmail.com>
2026-03-26 14:42:43 +0900

    reservation

--- src/main/java/com/example/demo/reservation/ReservationService.java
+++ src/main/java/com/example/demo/reservation/ReservationService.java
@@ -2,6 +2,9 @@ package com.example.demo.reservation;
 
 import com.example.demo.patient.Patient;
 import com.example.demo.patient.PatientRepository;
+import com.example.demo.security.security.CustomUserDetails;
+import com.example.demo.staff.Staff;
+import com.example.demo.staff.StaffRepository;
 import com.example.demo.user.User;
 import com.example.demo.user.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +17,7 @@ public class ReservationService {
     private final ReservationRepository reservationRepository;
     private final PatientRepository patientRepository;
     private final UserRepository userRepository;
+    private final StaffRepository staffRepository;
 
     public Long reservationReceived(ReservationDto reservationDto){
         CustomUserDetails userDetails = (CustomUserDetails) SecurityContextHolder
@@ -35,7 +39,15 @@ public class ReservationService {
         return reservation.getReservationId();
     }
 
-    public int reservationConfirmed(){
+    public Long reservationConfirmed(ReservationDto reservationDto){
+        Reservation reservation=reservationRepository.findById(reservationDto.getReservationId())
+                .orElseThrow(() -> new RuntimeException("Not exist"));
 
+        Staff staff=staffRepository.findById(reservationDto.getDoctorId())
+                .orElseThrow(() -> new RuntimeException("Not exist"));
+        reservation.setStaff(staff);
+        reservation.setReservationDatetime(reservationDto.getReservationDatetime());
+
+        return reservationDto.getReservationId();
     }
 }

eb3723df3c4c72f16217fff90cc486da668d7002
vlria <shslvllria540@gmail.com>
2026-03-26 10:09:55 +0900

    Security handler

--- src/main/java/com/example/demo/security/jwtutil/JWTUtil.java
+++ src/main/java/com/example/demo/security/jwtutil/JWTUtil.java
@@ -1,4 +1,4 @@
-package com.example.demo.auth.security.jwtutil;
+package com.example.demo.security.jwtutil;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;

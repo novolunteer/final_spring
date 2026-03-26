6cab9066652d5e9a77b44b5f953e4299a9e0c218
vlria <shslvllria540@gmail.com>
2026-03-26 10:09:55 +0900

    Security handler

---
+++ src/main/java/com/example/demo/security/handler/ApiLoginSuccessHandler.java
@@ -0,0 +1,43 @@
+package com.example.demo.security.handler;
+
+import com.example.demo.security.jwtutil.JWTUtil;
+import com.example.demo.user.UserDto;
+import com.google.gson.Gson;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class ApiLoginSuccessHandler implements AuthenticationSuccessHandler {
+    private final JWTUtil jwtUtil;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        UserDto userDto=(UserDto) authentication.getPrincipal();
+
+        Map<String,Object> claims=userDto.getClaims();
+
+        String accessToken=jwtUtil.generateToken(claims,5);
+        String refreshToken= jwtUtil.generateToken(claims,60*2);//2시간
+        claims.put("accessToken",accessToken);
+        claims.put("refreshToken",refreshToken);
+
+        Gson gson=new Gson();
+        String jsonStr=gson.toJson(claims);
+        response.setContentType("application/json;charset=utf-8");
+        PrintWriter pw= response.getWriter();
+        pw.println(jsonStr);
+        pw.close();
+    }
+}

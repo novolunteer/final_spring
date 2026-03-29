f55b7008c2a2bd3a3d199d140d8090e25fdbc871
gayeonsong <thdrkfl001@gmail.com>
2026-03-30 02:08:50 +0900

    스케줄타입완료

---
+++ src/main/java/com/example/demo/schedule/staff/repository/StaffScheduleRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.schedule.staff.repository;
+
+import com.example.demo.schedule.staff.entity.StaffSchedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+public interface StaffScheduleRepository extends JpaRepository<StaffSchedule, Integer> {
+    Optional<StaffSchedule> findByStaff_StaffIdAndWorkDate(Integer staffId, LocalDate workDate);
+
+    boolean existsByStaff_StaffIdAndWorkDate(Integer staffId, LocalDate workDate);
+}

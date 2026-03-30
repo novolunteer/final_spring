4682b096fd6bf24bd1853cea724151648ce8052d
novolunteeer <novolunteeer@gmail.com>
2026-03-30 16:18:10 +0900

    chat staff list

--- src/main/java/com/example/demo/user/UserRepository.java
+++ src/main/java/com/example/demo/user/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
@@ -13,4 +14,5 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> getWithRoles(@Param("email") String email);
 
     Optional<User> findByUserId(Integer userId);
+    List<User> findByUserIdIsNot(Integer userId);
 }

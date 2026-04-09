b21e3733d538c4ecf9b5304faf7020ecc2505ee7
novolunteeer <novolunteeer@gmail.com>
2026-04-09 16:35:09 +0900

    local join

--- src/main/java/com/example/demo/user/UserRepository.java
+++ src/main/java/com/example/demo/user/UserRepository.java
@@ -14,4 +14,5 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> getWithRoles(@Param("email") String email);
 
     Optional<User> findByUserId(Integer userId);
+    boolean existsByEmail(String email);
 }

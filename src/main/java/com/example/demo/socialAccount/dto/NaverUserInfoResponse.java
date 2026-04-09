810dee2f27cbabff6711fe812979d3f8be599083
novolunteeer <novolunteeer@gmail.com>
2026-04-09 18:16:51 +0900

    소셜 로그인 서비스에서 기존 유저 정보 찾는 메소드 만들기

---
+++ src/main/java/com/example/demo/socialAccount/dto/NaverUserInfoResponse.java
@@ -0,0 +1,21 @@
+package com.example.demo.socialAccount.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class NaverUserInfoResponse {
+    private String resultCode;
+    private String message;
+    private NaverUserResponse response;
+
+    @Getter
+    @Setter
+    public static class NaverUserResponse{
+        private String name;
+        private String gender;
+        private String mobile;
+        private String mobile_e164;
+    }
+}

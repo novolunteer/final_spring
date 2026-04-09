5b0e8f8592fb5b9ac002e87223e079adb74c8cc8
novolunteeer <novolunteeer@gmail.com>
2026-04-09 10:27:34 +0900

    billing amount

--- src/main/java/com/example/demo/billing/BillingController.java
+++ src/main/java/com/example/demo/billing/BillingController.java
@@ -8,9 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -35,6 +33,27 @@ public class BillingController {
         try{
             Page<BillingDto> billings=billingService.getBillingList(keyword, pageable, departmentId);
             return ResponseEntity.ok(billings);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PostMapping("/billing/total/amount")
+    public ResponseEntity<String> insertTotalAmount(@RequestBody BillingDto dto,
+                                                    @AuthenticationPrincipal CustomUserDetails details){
+        if (details == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        Integer departmentId=details.getDepartmentId();
+        if (departmentId == null){
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
+        try{
+            billingService.insertTotalAmount(dto, departmentId);
+            return ResponseEntity.ok("success");
         } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();

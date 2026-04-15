6c44f5312165908060b14dc770f3633d9f8edd4b
vlria <shslvllria540@gmail.com>
2026-04-15 10:43:40 +0900

    SSE

--- src/main/java/com/example/demo/security/filter/JWTCheckFilter.java
+++ src/main/java/com/example/demo/security/filter/JWTCheckFilter.java
@@ -36,7 +36,7 @@ public class JWTCheckFilter extends OncePerRequestFilter {
         String path=request.getRequestURI();
         if (path.startsWith("/none") || path.startsWith("/login") || path.startsWith("/logout") ||
                 path.startsWith("/join") || path.startsWith("/jwt/token/refresh") || path.startsWith("/ws")
-            || path.startsWith("/upload") || path.startsWith("/api/sse")){
+            || path.startsWith("/upload")){
             return true;
         }
 

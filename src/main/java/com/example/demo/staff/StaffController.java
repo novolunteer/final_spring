eb1efe88d5eeaf9b0fb975b6e0e260c541a02c10
gayeon <thdrkfl001@gmail.com>
2026-04-16 17:44:50 +0900

    SSE 수정중 ....

--- src/main/java/com/example/demo/staff/StaffController.java
+++ src/main/java/com/example/demo/staff/StaffController.java
@@ -1,8 +1,6 @@
 package com.example.demo.staff;
 
-import com.example.demo.staff.dto.StaffRegisterDto;
-import com.example.demo.staff.dto.StaffResponseDto;
-import com.example.demo.staff.dto.StaffUpdateDto;
+import com.example.demo.staff.dto.*;
 import com.example.demo.department.DepartmentDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -12,6 +10,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -24,6 +23,13 @@ public class StaffController {
     public ResponseEntity<?> registerStaff(@RequestBody StaffRegisterDto dto){
         Integer staffId = staffService.register(dto);
         return ResponseEntity.ok("직원등록완료 staffId="+staffId);
+    }
+
+    //엑셀 일괄등록
+    @PostMapping("/bulk-upload")
+    public ResponseEntity<StaffBulkUploadResponseDto> bulkUpload(@RequestBody List<StaffBulkUploadRequestDto> requestList){
+        StaffBulkUploadResponseDto responseDto= staffService.bulkUpload(requestList);
+        return ResponseEntity.ok(responseDto);
     }
 
     @GetMapping("/list")

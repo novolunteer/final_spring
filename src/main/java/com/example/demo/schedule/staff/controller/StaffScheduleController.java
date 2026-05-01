81a41ad7bdf6c07f5acda937438a87db8e62f00d
vlria-os <shslvllria540@gmail.com>
2026-05-01 15:52:30 +0900

    Update StaffScheduleController.java

--- src/main/java/com/example/demo/schedule/staff/controller/StaffScheduleController.java
+++ src/main/java/com/example/demo/schedule/staff/controller/StaffScheduleController.java
@@ -40,6 +40,7 @@ public class StaffScheduleController {
     public StaffScheduleDetailDto getMyScheduleDetail(
             @AuthenticationPrincipal CustomUserDetails userDetails,
             @RequestParam(required = false) LocalDate today){
+        System.out.println("=========>"+staffScheduleService.getMyDetail(userDetails.getUserId(), today));
         return staffScheduleService.getMyDetail(userDetails.getUserId(), today);
     }
 
